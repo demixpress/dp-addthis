d94497a9cf1789a23b9a717524faa5581fb3bdcd
DemixPress <demixpress@gmail.com>
2015-12-15 16:27:17 +0800

    change plugin description

--- dp-addthis.php
+++ dp-addthis.php
@@ -3,7 +3,7 @@
 /*
  * Plugin Name: (dp) AddThis
  * Plugin URI: http://demixpress.com/plugins/dp-addthis/
- * Description: Displays AddThis tools on your site.
+ * Description: Makes it even easier and better to display AddThis tools (share buttons, follow buttons and recommended contents) on your WordPress site.
  * Version: 1.0.1
  * Author: DemixPress
  * Author URI: http://demixpress.com/
